fdd4adb080f56a07e7fc999ceee768fbc5119d93
Li2nga <137508531+Li2nga@users.noreply.github.com>
2025-01-23 13:34:30 +0300

    task4 CalculateTagsWithoutArray

--- src/main/java/yandex/coderun/quickstart/task4/CalculateTagsWithoutArray.java
+++ src/main/java/yandex/coderun/quickstart/task4/CalculateTagsWithoutArray.java
@@ -6,6 +6,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+/**
+ * <a href="https://coderun.yandex.ru/selections/quickstart/problems/calculate-tags">task link</a><br>
+ * test - OK
+ */
 public class CalculateTagsWithoutArray {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
