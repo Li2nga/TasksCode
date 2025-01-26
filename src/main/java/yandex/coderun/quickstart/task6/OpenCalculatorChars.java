27b3d251c085f8fee9bea4f40867d91a997a527f
Li2nga <137508531+Li2nga@users.noreply.github.com>
2025-01-26 11:57:30 +0300

    task6 open calculator (char)

--- src/main/java/yandex/coderun/quickstart/task6/OpenCalculatorChars.java
+++ src/main/java/yandex/coderun/quickstart/task6/OpenCalculatorChars.java
@@ -6,21 +6,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
  * <a href="https://coderun.yandex.ru/selections/quickstart/problems/open-calculator">task link</a><br>
  * test - OK
  */
-public class OpenCalculator {
+public class OpenCalculatorChars {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] nums = reader.readLine().split(" ");
-        int x = Integer.parseInt(nums[0]);
-        int y = Integer.parseInt(nums[1]);
-        int z = Integer.parseInt(nums[2]);
+        String nums = reader.readLine();
+        char x = nums.charAt(0);
+        char y = nums.charAt(2);
+        char z = nums.charAt(4);
         String n = reader.readLine();
 
         writer.write(String.valueOf(countNewFigures(x, y, z, n)));
@@ -28,14 +29,10 @@ public class OpenCalculator {
         writer.close();
     }
 
-    static int countNewFigures(int x, int y, int z, String n) {
-        Set<Integer> set = new HashSet<>();
-        set.add(x);
-        set.add(y);
-        set.add(z);
-        char[] chars = n.toCharArray();
-        for (char aChar : chars) {
-            set.add(Integer.parseInt(String.valueOf(aChar)));
+    static int countNewFigures(char x, char y, char z, String n) {
+        Set<Character> set = new HashSet<>(List.of(x, y, z));
+        for (int i = 0; i < n.length(); i++) {
+            set.add(n.charAt(i));
         }
         return set.size() - 3;
     }
